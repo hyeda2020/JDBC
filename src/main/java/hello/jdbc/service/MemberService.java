d5700477f1108d14ae2f421a17fa5b7a9160beb6
hyeda2020 <hyeda2020@gmail.com>
2024-09-14 23:35:33 +0900

    트랜잭션 AOP 적용 - 서비스에서 트랜잭션 관련 로직 제외 가능

--- src/main/java/hello/jdbc/service/MemberService.java
+++ src/main/java/hello/jdbc/service/MemberService.java
@@ -4,43 +4,32 @@ package hello.jdbc.service;
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * 트랜잭션 - @Transactional AOP
+ */
 @Slf4j
 public class MemberService {
 
-    private final TransactionTemplate txTemplate;
     private final MemberRepositoryV3 memberRepository;
 
-    public MemberService(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
-        this.txTemplate = new TransactionTemplate(transactionManager);
+    public MemberService(MemberRepositoryV3 memberRepository) {
         this.memberRepository = memberRepository;
     }
 
+    @Transactional // 트랜잭션 AOP 적용
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        /**
-         * 트랜잭션 템플릿 적용
-         * 언체크 예외가 발생하면 롤백. 그 외의 경우 (체크 예외가 발생하여도) 커밋 -> 스프링 기본 원칙
-         * 덕분에 커밋/롤백 소스를 서비스에서 제외시킬 수 있다는 장점이 있지만,
-         * 여전히 트랜잭션을 처리하는 기술 로직이 서비스에 포함되어 있다는 단점 존재.
-         */
-        txTemplate.executeWithoutResult((status) -> {
-            try {
-                //비즈니스 로직
-                bizLogic(fromId, toId, money);
-            } catch (SQLException e) { // 체크 예외가 발생하면 언체크 예외를 던져서 롤백되도록
-                throw new IllegalStateException(e);
-            }
-        });
+        //비즈니스 로직
+        bizLogic(fromId, toId, money);
     }
     private void bizLogic(String fromId, String toId, int money)
             throws SQLException {
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
+
         memberRepository.update(fromId, fromMember.getMoney() - money);
         validation(toMember);
         memberRepository.update(toId, toMember.getMoney() + money);
@@ -48,16 +37,6 @@ public class MemberService {
     private void validation(Member toMember) {
         if (toMember.getMemberId().equals("ex")) {
             throw new IllegalStateException("이체중 예외 발생");
-        }
-    }
-    private void release(Connection con) {
-        if (con != null) {
-            try {
-                con.setAutoCommit(true); //커넥션 풀 고려
-                con.close();
-            } catch (Exception e) {
-                log.info("error", e);
-            }
         }
     }
 }
--- src/test/java/hello/jdbc/service/MemberServiceTest.java
+++ src/test/java/hello/jdbc/service/MemberServiceTest.java
@@ -3,35 +3,77 @@ package hello.jdbc.service;
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import hello.jdbc.connection.ConnectionConst;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static hello.jdbc.connection.ConnectionConst.*;
 import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
+@SpringBootTest
 public class MemberServiceTest {
 
+    @Autowired
     private MemberService memberService;
+    @Autowired
     private MemberRepositoryV3 memberRepository; // 트랜잭션 기능 O
-
-    @BeforeEach
-    void before() throws SQLException {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-        memberRepository = new MemberRepositoryV3(dataSource);
-        PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
-        memberService = new MemberService(transactionManager, memberRepository);
-    }
 
     @AfterEach
     void after() throws SQLException {
         memberRepository.deleteById("memberA");
         memberRepository.deleteById("memberB");
         memberRepository.deleteById("ex");
+    }
+
+    /**
+     * 테스트 클래스 안에서
+     * 다음과 같은 내부 설정 클래스를 만들고
+     * @TestConfiguration 애노테이션을 붙이면
+     * 스프링 부트가 자동으로 여기서 등록된 빈들을 추가로 등록해줌으로써
+     * 테스트에서 해당 빈들을 사용 가능
+     */
+    @TestConfiguration
+    static class TestConfig{
+        @Bean
+        DataSource dataSource() {
+            return new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        }
+
+        @Bean
+        PlatformTransactionManager transactionManager() {
+            return new DataSourceTransactionManager(dataSource());
+        }
+
+        @Bean
+        MemberRepositoryV3 memberRepositoryV3() {
+            return new MemberRepositoryV3(dataSource());
+        }
+
+        @Bean
+        MemberService memberService() {
+            return new MemberService(memberRepositoryV3());
+        }
+    }
+
+    @Test
+    @DisplayName("AOP Check")
+    void AopCheck() {
+        log.info("memberService class={}", memberService.getClass()); // MemberService에 프록시가 적용된 것 확인 가능
+        log.info("memberRepository class={}", memberRepository.getClass());
+        Assertions.assertThat(AopUtils.isAopProxy(memberService.getClass())).isTrue();
+        Assertions.assertThat(AopUtils.isAopProxy(memberRepository.getClass())).isTrue();
     }
 
     @Test
